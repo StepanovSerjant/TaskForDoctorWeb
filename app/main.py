d8b42f2789f6f942d5b5c8adc2039e1859a26595
Aleksey Stepanov <stpnvlks@gmail.com>
2020-07-13 20:24:01 +0300

    Some refactoring. Change upload_file func in main.py and get_hash in services.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import shutil
 import json
 from bottle import error, run, get, post, delete, request, response, static_file
@@ -11,6 +12,7 @@ from services import check_root, get_hash
 @get('/download')
 def get_file():
     """ Processing a file download request """
+
     file_hash = request.query.get('h')
     if not file_hash:
         return {'status': 'fail', 'error': 'have no h parameter'}
@@ -19,7 +21,6 @@ def get_file():
         find_dir = os.path.join(ROOT_PATH, file_hash[:2])
         file = [file for file in os.listdir(find_dir) if file.split('.')[0] == file_hash][0]
         return static_file(file, root=find_dir, download=file)
-
     except (IndexError, FileNotFoundError):
         return {'status': 'fail', 'error': 'file does not exists'}
 
@@ -27,30 +28,28 @@ def get_file():
 @post('/upload')
 def upload_file():
     """ Processing a file upload request """
+
     try:
         upload = request.files.get('file')
-        ext = os.path.splitext(upload.filename)[1]
-        save_to_get_hash = os.path.join(ROOT_PATH, upload.filename)
-        upload.save(save_to_get_hash)
+        file_hash = get_hash(upload)
+        save_dir = os.path.join(ROOT_PATH, file_hash[:2])
 
-        new_filename = get_hash(save_to_get_hash, 65536)
-        save_dir = os.path.join(ROOT_PATH, new_filename[:2])
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
 
-        shutil.move(save_to_get_hash, os.path.join(save_dir, new_filename + ext))
-        return {'result': 'success', 'message': 'file has been saved', 'hash': new_filename}
-    
+        upload.save(os.path.join(save_dir, file_hash + Path(upload.filename).suffix))
+        return {'result': 'success', 'message': 'file has been saved', 'hash': file_hash}
     except OSError:
-        return {'result': 'fail', 'error': 'file exists'}
+        return {'result': 'unsuccess', 'message': 'file exists'}
 
     except AttributeError:
-        return  {'result': 'fail', 'error': 'there is no file key or cannot find the file'}
+        return  {'result': 'unsuccess', 'message': 'there is no file key or cannot find the file'}
 
 
 @delete('/remove')
 def remove_file():
     """ Processing a request to delete a file """
+
     file_hash = request.query.get('h')
     if not file_hash:
         return {'result': 'fail', 'error': 'there is no parameter named h'}
