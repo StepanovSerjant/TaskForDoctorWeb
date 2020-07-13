9eb77df10caa208ae2c80cf82eebc184f248e7d5
Aleksey Stepanov <stpnvlks@gmail.com>
2020-07-13 20:22:43 +0300

    Some refactoring. Change upload_file func in main.py and get_hash in services.py

--- app/services.py
+++ app/services.py
@@ -5,28 +5,28 @@ from config import ROOT_PATH
 
 
 def get_hash(filepath, bufsize):
-	""" Hash function of the file 
+    """ Hash function of the file 
 
     :param filepath: path to the file.
     :type filepath: str
 
     :param bufsize: chunk to get the hash of the file.
     :type bufsize: int
-	"""
-	lib = hashlib.sha384()
+    """
+    lib = hashlib.sha384()
 
-	with open(filepath, 'rb') as f:
-		while True:
-			data = f.read(bufsize)
-			if not data:
-				break
-			lib.update(data)
+    with open(filepath, 'rb') as f:
+        while True:
+            data = f.read(bufsize)
+            if not data:
+                break
+            lib.update(data)
 
-	return lib.hexdigest()
+    return lib.hexdigest()
 
 
 def check_root():
-	""" Function for checking the presence of a folder for saving files """
-	if not os.path.exists(ROOT_PATH):
-		os.mkdir(ROOT_PATH)
+    """ Function for checking the presence of a folder for saving files """
+    if not os.path.exists(ROOT_PATH):
+        os.mkdir(ROOT_PATH)
 
